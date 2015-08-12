4ff300c2d084ed02f0f2d0b0c3701c4918dae697
pronin <denis.pronin@sibers.com>
2015-08-12 15:45:52 +0600

    record

--- www/js/record/recordCtrl.js
+++ www/js/record/recordCtrl.js
@@ -1,6 +1,51 @@
 angular.module('voicebaseRecord')
-    .controller('recordCtrl', ['$scope', '$state', 'tokensApi', function ($scope, $state, tokensApi) {
+    .controller('recordCtrl', ['$scope', '$state', '$ionicPlatform', 'tokensApi', function ($scope, $state, $ionicPlatform, tokensApi) {
 
+        $scope.isRecord = false;
 
+        $scope.toggleRecord = function () {
+            $ionicPlatform.ready(function () {
+                $scope.isRecord = !$scope.isRecord;
+
+                if($scope.isRecord) {
+                    $scope.startRecord();
+                }
+                else {
+                    $scope.stopRecord();
+                }
+            });
+        };
+
+        var mediaRec;
+        var src = 'vbsRecording.wav';
+
+        $scope.startRecord = function () {
+            console.log('startRecord');
+
+            if(mediaRec) {
+                mediaRec.release();
+            }
+
+            mediaRec = new Media(src, onMediaCallSuccess, onMediaCallError);
+            mediaRec.startRecord();
+        };
+
+        $scope.stopRecord = function () {
+            console.log('stopRecord');
+            mediaRec.stopRecord();
+        };
+
+        $scope.playRecord = function () {
+            if(mediaRec) {
+                mediaRec.play();
+            }
+        };
+
+        function onMediaCallSuccess() {
+            console.log("***test: new Media() succeeded ***");
+        }
+        function onMediaCallError(error) {
+            console.log("***test: new Media() failed ***");
+        }
 
     }]);
