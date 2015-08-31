f805153a0d12fc19629987b59bdf6eab0d289833
pronin <denis.pronin@sibers.com>
2015-08-31 15:27:03 +0600

    validation token on login form

--- www/js/login/loginCtrl.js
+++ www/js/login/loginCtrl.js
@@ -1,11 +1,25 @@
 angular.module('voicebaseRecord')
-    .controller('loginCtrl', ['$scope', '$state', 'tokensApi', function ($scope, $state, tokensApi) {
+    .controller('loginCtrl', [
+        '$scope',
+        '$state',
+        'tokensApi',
+        function ($scope, $state, tokensApi) {
 
-        $scope.token = '';
+            $scope.form = {
+                formValid: true,
+                token: ''
+            };
 
-        $scope.signIn = function () {
-            tokensApi.setToken($scope.token);
-            $state.go("record");
-        };
+            $scope.signIn = function () {
+                if($scope.form.token) {
+                    $scope.form.formValid = true;
+                    tokensApi.setToken($scope.form.token);
+                    $state.go("record");
+                }
+                else {
+                    $scope.form.formValid = false;
+                }
+            };
 
-    }]);
+        }
+    ]);
